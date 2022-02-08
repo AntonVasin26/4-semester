7ab7bb4fa72a71e2fd59995f0fb15c68866d9e98
AntonVasin26 <vasin.anton.work@gmail.com>
2022-02-09 02:28:05 +0300

    1

---
+++ Seminar 1/Homework 1/Homework 1/ex4.cpp
@@ -0,0 +1,93 @@
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+public:
+	using clock_t = std::chrono::steady_clock;
+	using time_point_t = clock_t::time_point;
+	using interval = clock_t::duration;
+	Timer() : m_begin(clock_t::now())
+	{}
+	void start()
+	{
+		m_begin = clock_t::now();
+		flag = false;
+		time = std::chrono::microseconds(0);
+	};
+
+	void play()
+	{
+		if (flag == true)
+		{
+			m_begin = clock_t::now();
+			flag = false;
+		}
+	};
+
+	void stop()
+	{
+		if (flag == false)
+		{
+			m_end = clock_t::now();
+			time += (m_end - m_begin);
+			flag = true;
+		}
+	};
+
+
+	void print_time()
+	{
+		if (flag == false)
+		{
+			m_end = clock_t::now();
+			std::cout
+				<< std::chrono::duration_cast <std::chrono::seconds> (m_end - m_begin).count() << " second:   "
+				<< std::chrono::duration_cast <std::chrono::microseconds> (m_end - m_begin).count() << " microsecond\n";
+			m_begin = m_end;
+		}
+		else
+		{
+			std::cout
+				<< std::chrono::duration_cast <std::chrono::seconds> (time).count() << " second:   "
+				<< std::chrono::duration_cast <std::chrono::microseconds> (time).count() << " microsecond\n";
+		}
+	}
+
+	~Timer() noexcept
+	{}
+private:
+	time_point_t m_begin;
+	time_point_t m_end;
+	interval time = std::chrono::microseconds(0);
+	bool flag = false;
+};
+
+int main(int argc, char** argv)
+{
+	{
+		Timer t1;
+		Timer t2;
+		Timer t3;
+		auto c = 1.0;
+		t1.start();
+		for (auto j = 0; j < 10; ++j)
+		{
+			t2.play();
+			for (auto i = 0; i < 10000000; ++i)
+			{
+				t3.play();
+				c += 1.01;
+				c = std::pow(c, 1.01);
+				t3.stop();
+			}
+			t2.stop();
+			t2.print_time();
+			t3.print_time();
+		}
+		t1.print_time();
+		t2.print_time();
+		t3.print_time();
+	}
+
+}
