3b005c0ddef1bd2e483f211077a25015268b2c90
AntonVasin26 <vasin.anton.work@gmail.com>
2022-02-21 02:47:56 +0300

    update ex3

--- Seminar 2/Homework 2/Homework 2/test.cpp
+++ Seminar 2/Homework 2/Homework 2/test.cpp
@@ -6,27 +6,60 @@
 #include <forward_list>
 #include <chrono>
 #include <algorithm>
+#include <iomanip>
 
+#include <boost/multi_array.hpp>
+
+template < auto N, typename Container, typename Forward_Iterator >
+void fill_shape(const Container& container, Forward_Iterator shape)
+{
+	*shape = std::size(container);
+
+	if constexpr (N > 1)
+	{
+		fill_shape < N - 1 >(*(std::begin(container)), ++shape);
+	}
+}
 
 int main()
 {
-	std::vector<int> v1 = { 1,2,3,4 };
-	std::array<int, 6> a1 = { 5,6,7,8 };
-	for (auto i = 0; i < v1.size(); ++i)
-	{
-		std::cout << v1[i];
-	}
-	std::cout << '\n';
-	for (auto i = 0; i < a1.size(); ++i)
-	{
-		std::cout << a1[i];
-	}
-	std::cout << '\n';
-	v1.assign(a1.begin(), a1.end());
-	for (auto i = 0; i < v1.size(); ++i)
-	{
-		std::cout << v1[i];
-	}
-	std::cout << '\n';
+	const auto size_1 = 3U;
+	const auto size_2 = 4U;
+	const auto size_3 = 5U;
 
+	std::vector < std::vector < std::vector < int > > > v(size_1,
+		std::vector < std::vector < int > >(size_2,
+			std::vector < int >(size_3, 0)));
+	auto counter = 0;
+
+	for (auto i = 0U; i < size_1; ++i)
+	{
+		for (auto j = 0U; j < size_2; ++j)
+		{
+			for (auto k = 0U; k < size_3; ++k)
+			{
+				(v[i][j][k] = ++counter);
+			}
+		}
+	}
+
+	std::vector < boost::multi_array < int, 3 >::index > shape(3, boost::multi_array < int, 3 >::index(0));
+	//for (auto element : shape)
+	//{
+	//	std::cout << element << ' ';
+	//}
+	fill_shape < 3 >(v, std::begin(shape));
+	boost::multi_array < int, 3 > multi_array(shape);
+	for (auto element1 : multi_array)
+	{
+		for (auto element2 : element1)
+		{
+			for (auto& element3 : element2)
+			{
+				std::cout << element3 << ' ';
+			}
+			std::cout << '\n';
+		}
+		std::cout << '\n';
+	}
 }
