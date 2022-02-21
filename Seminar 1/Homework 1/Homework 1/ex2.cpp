129129b240e99f3397c5d1136bed0d49c27fb809
AntonVasin26 <vasin.anton.work@gmail.com>
2022-02-21 16:20:13 +0300

    ex2 create

--- Seminar 1/Homework 1/Homework 1/ex2.cpp
+++ Seminar 1/Homework 1/Homework 1/ex2.cpp
@@ -1,19 +1,2 @@
 //ex2 create successfully
-//change in branch 1
-//base2
-//base3
-//base4
-//base5
-//change in master 2
-//base7
-//base8
-//change in branch 3
-//ex2 second part
-//change in branch 4
-//base10
-//change in branch 5
-//base12
-//base13
-//base14
-//base15
-//change in branch 6
+
