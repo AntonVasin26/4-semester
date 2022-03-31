6fcbbad24ddbcabb4a19f5ea108c94b530194063
AntonVasin26 <vasin.anton.work@gmail.com>
2022-03-31 17:11:10 +0300

    Сonvert_unix_in_date

--- Seminar 6/Homework 6/Homework 6/ex2.cpp
+++ Seminar 6/Homework 6/Homework 6/ex2.cpp
@@ -6,16 +6,18 @@
 
 int main()
 {
-	long int n_str = 4;
-	const long int l = 23;
-	char ch[23];
-	//std::string line;
+	long int n_str;
+	const long int l = 23; //Number of characters
 	std::fstream fin("for_ex2.txt", std::ios::in);
-	//std::cin >> n_str;
-	fin.seekg((l+2)*n_str);
-	fin.get(ch, l);
-	//std::getline(fin, line);
-	std::cout << ch;
-	//std::cout << line;
-	
+	std::cout << "Enter the line number 0-9: ";
+	std::cin >> n_str;
+	fin.seekg((l+2)*n_str); //Why plus 2? Where does the second character come from?
+							//The first one is "Enter", and what other symbol?
+
+	std::string line;
+	std::getline(fin, line);
+	std::cout << line;
+	//char ch[24];
+	//fin.get(ch, l+1);
+	//std::cout << ch;
 }
