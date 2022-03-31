0f4d1d7bc1007bfccdf940b4abf8d4a36e07a9ef
AntonVasin26 <vasin.anton.work@gmail.com>
2022-04-01 00:09:17 +0300

    Homework 5/6

--- test/test/Sourc.cpp
+++ test/test/Sourc.cpp
@@ -1,116 +1,40 @@
 #include <iostream>
-#include <chrono>
-#include <iomanip>
+#include <regex>
+#include <string>
 #include <fstream>
-#include <filesystem>
-#include <time.h>
-#include <unordered_map>
 
-std::string get_ext(const std::filesystem::path& file) {
-	std:: string name = file.filename().string();
-	size_t pos = name.rfind('.');
-	if (pos == SIZE_MAX) return "";
-	return name.substr(pos + 1, std::string::npos);
-}
 
-std::string get_type(const std::filesystem::path& file)
+
+int main()
 {
-	std::string ext = get_ext(file);
-	std::unordered_map <std::string, std::string> type = {
-		{"drv", "system file"},
-		{"sys", "system file"},
-		{"exe", "system file"},
-		{"txt", "text file"},
-		{"rtf", "text file"},
-		{"doc", "text file"},
-		{"docx", "text file"},
-		{"odt", "text file"},
-		{"json", "text file"},
-		{"bmp", "graphic file"},
-		{"gif", "graphic file"},
-		{"jpg", "graphic file"},
-		{"tif", "graphic file"},
-		{"png", "graphic file"},
-		{"pds", "graphic file"},
-		{"htm", "Web page"},
-		{"html", "Web page"},
-		{"wav", "audio file"},
-		{"mp3", "audio file"},
-		{"midi", "audio file"},
-		{"kar", "audio file"},
-		{"ogg", "audio file"},
-		{"avi", "video file"},
-		{"mpeg", "video file"},
-		{"zip", "arñhive file"},
-		{"xls", "spreadsheet"},
-		{"ods", "spreadsheet"},
-		{"cpp", "text file(for_code)"},
-	};
-	if (type.find(ext) == type.end()) return "other extension";
-	return type[ext];
-}
+	std::cout << "ex4\n\n";
+	std::regex pattern(R"((?:(?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])|(?:(?:[0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})\.(?:(?:(?:0[13578]|1[02])\.(?:0[1-9]|[12][0-9]|3[01]))|(?:(?:0[469]|11)\.(?:0[1-9]|[12][0-9]|30))|(?:02\.(?:0[1-9]|[1][0-9]|2[0-8]))))|(?:(?:(?:[0-9]{2})(?:0[48]|[2468][048]|[13579][26])|(?:(?:0[48]|[2468][048]|[3579][26])00))\.02\.29))");
+										//time							/data
+	//Output all the times and dates in the file one after another,
+	//taking into account leap years, but you need to write zeros
+	//before the numbers.
+	std::regex pattern1(R"((?:(?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])|(?:(?:(?:[0-9]{3})?[1-9]|(?:[0-9]{2})?[1-9][0-9]{1}|(?:[0-9]{1})?[1-9][0-9]{2}|[1-9][0-9]{3})\.(?:(?:(?:0[13578]|1[02])\.(?:0[1-9]|[12][0-9]|3[01]))|(?:(?:0[469]|11)\.(?:0[1-9]|[12][0-9]|30))|(?:02\.(?:0[1-9]|[1][0-9]|2[0-8]))))|(?:(?:(?:[0-9]{2})?(?:0?[48]|[2468][048]|[13579][26])|(?:(?:0[48]|[2468][048]|[3579][26])00))\.02\.29))");
+	//now distinguishes the years in the style: XXXX, XXX, XX, X
+	std::regex pattern2(R"((?:(?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])|(?:(?:(?:[0-9]{3})?[1-9]|(?:[0-9]{2})?[1-9][0-9]{1}|(?:[0-9]{1})?[1-9][0-9]{2}|[1-9][0-9]{3})\.(?:(?:(?:0?[13578]|1[02])\.(?:0[1-9]|[12][0-9]|3[01]))|(?:(?:0?[469]|11)\.(?:0[1-9]|[12][0-9]|30))|(?:0?2\.(?:0[1-9]|[1][0-9]|2[0-8]))))|(?:(?:(?:[0-9]{2})?(?:0?[48]|[2468][048]|[13579][26])|(?:(?:0[48]|[2468][048]|[3579][26])00))\.0?2\.29))");
+	//now distinguishes the month in the style: XX, X
+	std::regex pattern3(R"((?:(?:[01]?[0-9]|2[0-3]):[0-5]?[0-9]:[0-5]?[0-9])|(?:(?:(?:[0-9]{2})?(?:0?[48]|[2468][048]|[13579][26])|(?:(?:0?[48]|[2468][048]|[13579][26])00))\.0?2\.29)|(?:(?:(?:[0-9]{3})?[1-9]|(?:[0-9]{2})?[1-9][0-9]{1}|(?:[0-9]{1})?[1-9][0-9]{2}|[1-9][0-9]{3})\.(?:(?:(?:0?[13578]|1[02])\.(?:[12][0-9]|3[01]|0?[1-9]))|(?:(?:0?[469]|11)\.(?:[12][0-9]|30|0?[1-9]))|(?:0?2\.(?:[1][0-9]|2[0-8]|0?[1-9])))))");
+	std::smatch matches;
+	//getline(std::cin, line);
+	std::fstream fin("ex.txt", std::ios::in);
+	std::string code{
+		std::istreambuf_iterator < char >(fin),
+		std::istreambuf_iterator < char >() };
+	std::sregex_iterator begin(code.cbegin(), code.cend(), pattern3);
+	std::sregex_iterator end;
 
-std::string get_unix_type(std::filesystem::path& file)
-{
-	using namespace std::filesystem;
-	if (is_directory(file)) return "directory";
-	if (is_regular_file(file)) return "regular file";
-	if (is_character_file(file)) return "character device";
-	if (is_block_file(file)) return "block device";
-	if (is_socket(file)) return "named IPC socket";
-	if (is_fifo(file)) return "named pipe";
-	if (is_symlink(file)) return "symbolic link";
-	return "other file";
-}
-
-template <typename TP>
-std::time_t to_time_t(TP tp)
-{
-	using namespace std::chrono;
-	auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now()
-		+ system_clock::now());
-	return system_clock::to_time_t(sctp);
-}
-
-void view_directory(const std::filesystem::path& path)
-{
-	if (std::filesystem::exists(path))
-	{
-		if (std::filesystem::is_directory(path))
+	std::for_each(begin, end, [](const std::smatch& m)
 		{
-			std::cout << "directory: " << path.filename().string() << std::endl; 
-			for (const auto& entry : std::filesystem::directory_iterator(path))
+			for (std::size_t i = 0; i < m.size(); ++i)
 			{
-				auto file = entry.path();
-				auto file_name = file.filename().string();
-				auto ftime = std::filesystem::last_write_time(file);
-				auto unix = to_time_t<std::chrono::time_point<std::chrono::file_clock>>(ftime);
-				std::cout << file_name << '(' << get_unix_type(file) << "; " << get_type(file) << ") " << unix << std::endl;
+				std::cout << m[i] << " | ";
 			}
-		}
-		else
-		{
-			auto file = path;
-			auto file_name = file.filename().string();
-			auto ftime = std::filesystem::last_write_time(file);
-			auto unix = to_time_t<std::chrono::time_point<std::chrono::file_clock>>(ftime);
-			std::cout << file_name << '(' << get_unix_type(file) << "; " << get_type(file) << ") " << unix << std::endl;
-		}
-	}
-	else std::cout << "file not found" << std::endl;
+
+			std::cout << std::endl;
+		});
 }
 
-int main(int argc, char** argv)
-{
-	system("chcp 1251");
-
-	view_directory(std::filesystem::current_path());
-	std::cout << std::endl;
-	view_directory(std::filesystem::current_path()/"output.txt");
-	std::cout << std::endl;
-	view_directory(std::filesystem::current_path() / "outfit.txt");
-
-	system("pause");
-
-	return EXIT_SUCCESS;
-}
